e5353b1f70223ac75f262b37c090a6cf342214f7
tunchasan <hasantunc_ogr@outlook.com>
2020-08-05 23:36:34 +0300

    Add new components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React, { useState, useEffect } from "react";
 import InfoBox from "./InfoBox";
-import { FormControl, MenuItem, Select } from "@material-ui/core";
+import Table from "./Table";
+import Map from "./Map";
+import {
+  FormControl,
+  MenuItem,
+  Select,
+  Card,
+  CardContent,
+} from "@material-ui/core";
 import "./App.css";
 
 // https://disease.sh/v3/covid-19/countries
@@ -8,6 +16,16 @@ import "./App.css";
 function App() {
   const [countries, setCountries] = useState([]);
   const [selectedCountry, setSelectedCountry] = useState("worldwide");
+  const [countryInfo, setCountryInfo] = useState({});
+  const [tableData, setTableData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/all")
+      .then((response) => response.json())
+      .then((data) => {
+        setCountryInfo(data);
+      });
+  }, []);
 
   useEffect(() => {
     const getCountriesData = async () => {
@@ -18,6 +36,7 @@ function App() {
             name: country.country,
             value: country.countryInfo.iso2,
           }));
+          setTableData(data);
           setCountries(countries);
         });
     };
@@ -28,36 +47,68 @@ function App() {
     const countryName = event.target.value;
 
     setSelectedCountry(countryName);
-  };
 
+    const url =
+      countryName === "worldwide"
+        ? "https://disease.sh/v3/covid-19/all"
+        : `https://disease.sh/v3/covid-19/countries/${countryName}`;
+
+    await fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setSelectedCountry(countryName);
+        setCountryInfo(data);
+      });
+  };
+  console.log("XXXXX:", countryInfo);
   return (
     <div className="app">
-      <div className="app_header">
-        <h1>COVID19 TRACKER</h1>
-        <FormControl className="app_dropdown">
-          <Select
-            variant="outlined"
-            value={selectedCountry}
-            onChange={onCountryChange}
-          >
-            <MenuItem value="worldwide">Worldwide</MenuItem>
-            {countries.map((country) => (
-              <MenuItem value={country.value}>{country.name}</MenuItem>
-            ))}
-          </Select>
-        </FormControl>
+      <div className="app_left">
+        <div className="app_header">
+          <h1>COVID19 TRACKER</h1>
+          <FormControl className="app_dropdown">
+            <Select
+              variant="outlined"
+              value={selectedCountry}
+              onChange={onCountryChange}
+            >
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+
+        <div className="app_stats">
+          <InfoBox
+            title="Coronovirus Cases"
+            cases={countryInfo.todayCases}
+            total={countryInfo.cases}
+          />
+          <InfoBox
+            title="Recovered"
+            cases={countryInfo.todayRecovered}
+            total={countryInfo.recovered}
+          />
+          <InfoBox
+            title="Deaths"
+            cases={countryInfo.todayDeaths}
+            total={countryInfo.deaths}
+          />
+        </div>
+        <Map />
+        {/* Map */}
       </div>
 
-      <div className="app_stats">
-        <InfoBox title="Coronovirus Cases" cases={12353} total={2000} />
-        <InfoBox title="Recovered" cases={12345} total={2000} />
-        <InfoBox title="Deaths" cases={1235} total={200} />
-      </div>
-
-      {/* Table */}
-      {/* Graph */}
-
-      {/* Map */}
+      <Card className="app_right">
+        <CardContent>
+          <h3>Live Cases by Country</h3>
+          <Table countries={tableData} />
+          <h3>Worldwide new Cases</h3>
+          {/* Graph */}
+        </CardContent>
+      </Card>
     </div>
   );
 }
---
+++ src/Table.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Table() {
+  return <div></div>;
+}
+
+export default Table;
